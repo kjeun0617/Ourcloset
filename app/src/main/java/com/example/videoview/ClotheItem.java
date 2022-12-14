a928c7b455ae5bc853183cbb7f69394836d42dd2
kjeun0617 <117703830+kjeun0617@users.noreply.github.com>
2022-12-14 16:39:54 +0900

    하나의 객체로 만듦

---
+++ app/src/main/java/com/example/videoview/ClotheItem.java
@@ -0,0 +1,26 @@
+public class ClotheItem {
+
+    private String name;
+    private int image;
+
+    public ClotheItem(String name, int image) {
+        this.name = name;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
