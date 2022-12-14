7f664bf2a7afbc758f5d070b190f0faca5ab0142
kjeun0617 <117703830+kjeun0617@users.noreply.github.com>
2022-12-14 16:17:58 +0900

    Update closetlist.java

--- app/src/main/java/com/example/videoview/closetlist.java
+++ app/src/main/java/com/example/videoview/closetlist.java
@@ -8,6 +8,34 @@ public class Closetlist extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.closetlist);
- 
+    }
+}
+
+public class ClosetItem {
+
+    private String name;
+    private String tel;
+    private int image;
+
+    public SingerItem(String name, String tel, int image) {
+        this.name = name;
+        this.tel = tel;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
     }
 }
