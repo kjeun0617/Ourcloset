9bfe738415b02c3823793e648e58a6b30143a818
kjeun0617 <117703830+kjeun0617@users.noreply.github.com>
2022-12-14 16:06:17 +0900

    Update closetlist2.java

--- app/src/main/java/com/example/videoview/closetlist2.java
+++ app/src/main/java/com/example/videoview/closetlist2.java
@@ -1,1 +1,29 @@
+public class closetlist2 extends LinearLayout {  //list 객체 list2 모아줌
 
+    TextView textView;
+    ImageView imageView;
+    public closetlist2(Context context) {
+        super(context);
+
+        init(context);
+    }
+
+    public closetlist2(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        init(context);
+    }
+
+    public void init(Context context){
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.closetlist,this,true);
+
+        textView = (TextView)findViewById(R.id.textView);
+        imageView = (ImageView) findViewById(R.id.imageView);
+    }
+
+    public void setItem(Closetlist closetlist){
+        textView.setText(closetlist.getName());
+        imageView.setImageResource(closetlist.getImage());
+    }
+}
