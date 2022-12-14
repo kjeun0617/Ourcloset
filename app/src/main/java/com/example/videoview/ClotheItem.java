978a6d0018faf21279ae4489d73e8fbde767e4a5
kjeun0617 <117703830+kjeun0617@users.noreply.github.com>
2022-12-14 22:57:11 +0900

    Update ClotheItem.java

--- app/src/main/java/com/example/videoview/ClotheItem.java
+++ app/src/main/java/com/example/videoview/ClotheItem.java
@@ -1,10 +1,12 @@
 public class ClotheItem {
 
     private String name;
+    private String locate;
     private int image;
 
-    public ClotheItem(String name, int image) {
+    public SingerItem(String name, String locate, int image) {
         this.name = name;
+        this.locate = tel;
         this.image = image;
     }
 
@@ -14,6 +16,14 @@ public class ClotheItem {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getLocate() {
+        return locate;
+    }
+
+    public void setLocate(String locate) {
+        this.locate = locate;
     }
 
     public int getImage() {
