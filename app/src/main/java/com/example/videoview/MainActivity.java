e986b3ce0e93b0c4c60766d0eaff9fc0c496d8b5
kjeun0617 <117703830+kjeun0617@users.noreply.github.com>
2022-12-14 14:56:54 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/videoview/MainActivity.java
+++ app/src/main/java/com/example/videoview/MainActivity.java
@@ -7,6 +7,11 @@ import android.os.Bundle;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+
 public class MainActivity extends AppCompatActivity {
 
     private VideoView videoView; // 비디오를 실행할 수 있게 도와주는 뷰
@@ -24,7 +29,24 @@ public class MainActivity extends AppCompatActivity {
         Uri uri = Uri.parse(videoURL);
         videoView.setMediaController(mediaController); // 미디어 제어 버튼부 세팅
         videoView.setVideoURI(uri); // 비디오 뷰의 주소를 설정
-        videoView.start(); // 비디오 실행!
-
+        videoView.start(); // 비디오 실행!\
     }
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+ 
+ 
+        //'내 옷장 보기' 버튼 클릭, 화면 전환
+        Button closetlist_btn = (Button) findViewById(R.id.closetlist_btn);
+        closetlist_btn.setOnClickListener(new View.OnClickListener(){
+ 
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), Closetlist.class);
+                startActivity(intent);
+            }
+        });
 }
+ 
