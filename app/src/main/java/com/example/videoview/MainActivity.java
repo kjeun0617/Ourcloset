5ae22bc4f79231ea063be8aef4af27d9c79b8a53
kjeun0617 <117703830+kjeun0617@users.noreply.github.com>
2022-12-14 17:27:44 +0900

    MainActivityJava 버튼 코드 오류 있음

--- app/src/main/java/com/example/videoview/MainActivity.java
+++ app/src/main/java/com/example/videoview/MainActivity.java
@@ -7,9 +7,9 @@ import android.os.Bundle;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
-/* import android.content.Intent;
+import android.content.Intent;
 import android.view.View;
-import android.widget.Button; */
+import android.widget.Button;
 
 
 public class VideoPlay extends AppCompatActivity {
@@ -29,25 +29,28 @@ public class VideoPlay extends AppCompatActivity {
         Uri uri = Uri.parse(videoURL);
         videoView.setMediaController(mediaController); // 미디어 제어 버튼부 세팅
         videoView.setVideoURI(uri); // 비디오 뷰의 주소를 설정
-        videoView.start(); // 비디오 실행!\
+        videoView.start(); // 비디오 실행!
     }
     
-    /* @Override
+    
+public class ChangeScreen extends AppCompatActivity{
+    
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.closet_main);
  
  
         //'내 옷장 보기' 버튼 클릭, 화면 전환
-        Button closetlist_btn = (Button) findViewById(R.id.closetlist_btn);
+        Button closetlist_btn = (Button) findViewById(R.id.closetlist_btn);  // button id : closetlist
         closetlist_btn.setOnClickListener(new View.OnClickListener(){
  
             @Override
             public void onClick(View view){
-                Intent intent = new Intent(getApplicationContext(), Closetlist.class);
+                Intent intent = new Intent(getApplicationContext(), id.class);  //class?
                 startActivity(intent);
             }
-        }); */
+        });
 }
  
 
